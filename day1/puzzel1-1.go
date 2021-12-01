7498407343ee70fb1823216d8ff0526472744a39
Christian Drüke <drueke@tagitron.de>
2021-12-01 13:07:20 +0100

    Day 1 - Part Two

--- day1/puzzel1-1.go
+++ day1/puzzel1-1.go
@@ -52,5 +52,27 @@ func PartOne() {
 	}
 
 	fmt.Println("Answer:", count)
+}
+
+func PartTwo() {
+	fmt.Println("Advent of Code Puzzel 1.2")
+	fmt.Println("Question: How many sums are larger than the previous sum?")
+
+	// Read input
+	depths, err := readInputNumber("day1/input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	count := 0
+	for i := 3; i < len(depths); i++ {
+		sum1 := depths[i-3] + depths[i-2] + depths[i-1]
+		sum2 := depths[i-2] + depths[i-1] + depths[i]
+		if sum1 < sum2 {
+			count++
+		}
+	}
+
+	fmt.Println("Answer:", count)
 
 }
--- main.go
+++ main.go
@@ -3,5 +3,6 @@ package main
 import "drueke.biz/advent-of-code-2021/day1"
 
 func main() {
-	day1.PartOne()
+	// day1.PartOne()
+	day1.PartTwo()
 }
