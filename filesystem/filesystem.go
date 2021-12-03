b4100f73275f5b0e1fde62e184920eb4cd6538f8
Nobody <topmailcat@googlemail.com>
2021-12-03 19:40:44 +0100

    add filesystem package for filesystem operations

---
+++ filesystem/filesystem.go
@@ -0,0 +1,28 @@
+package filesystem
+
+import (
+	"bufio"
+	"log"
+	"os"
+)
+
+func ReadInputLines(inputFile string) ([]string, error) {
+	// Open file
+	file, err := os.Open(inputFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	var line string
+	var lines []string
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		// Get line
+		line = scanner.Text()
+		lines = append(lines, line)
+	}
+
+	return lines, err
+}
