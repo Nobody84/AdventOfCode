baedda36c2b61ed73b6b766e531040680989d6a2
Nobody <topmailcat@googlemail.com>
2021-12-07 20:40:31 +0100

    refactoring Day 1

--- day1/day1.go
+++ day1/day1.go
@@ -1,82 +1,54 @@
 package day1
 
 import (
-	"fmt"
-	"io"
 	"log"
-	"os"
+	"nobody84/advent-of-code-2021/filesystem"
+	"strconv"
 )
 
-func readInputNumber(inputFile string) ([]int, error) {
-	// Open file
-	file, err := os.Open(inputFile)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
-
-	var perline int
-	var nums []int
-
-	for {
-		_, err := fmt.Fscanf(file, "%d\n", &perline)
-		if err != nil {
-			if err == io.EOF {
-				break // stop reading the file
-			}
-			fmt.Println(err)
-			os.Exit(1)
-		}
-
-		nums = append(nums, perline)
-	}
-
-	return nums, err
-}
-
-func getSummedValues(data []int, windowsSize int) ([]int, error) {
-	var result []int
-	for i := windowsSize - 1; i < len(data); i++ {
-		sum := 0
-		for p := 0; p < windowsSize; p++ {
-			sum = sum + data[i-p]
-		}
-
-		result = append(result, sum)
-	}
-
-	return result, nil
-}
-
-func getNumberOfPositiveGradients(data []int) (int, error) {
-	count := 0
-	for i := 1; i < len(data); i++ {
-		if data[i-1] < data[i] {
-			count++
-		}
-	}
-
-	return count, nil
-}
-
-func Solve() {
-	// Read input
-	deeps, err := readInputNumber("day1/input.txt")
+func PartOne(puzzelInput string) int {
+	// Read input lines
+	inputLines, err := filesystem.ReadInputLines(puzzelInput)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// Part One
-	fmt.Print("Day 1 - Part One: How many measurements are larger than the previous measurement? ")
+	depths := getDepth(inputLines)
+	return getNumberOfPositiveGradients(depths, 1)
+}
 
-	answer1, _ := getNumberOfPositiveGradients(deeps)
-	fmt.Println(fmt.Sprintf("Answer: [%d]", answer1))
+func PartTwo(puzzelInput string) int {
+	// Read input lines
+	inputLines, err := filesystem.ReadInputLines(puzzelInput)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	// Part Two
-	fmt.Print("Day 1 - Part Two: How many sums are larger than the previous sum? ")
+	depths := getDepth(inputLines)
+	return getNumberOfPositiveGradients(depths, 3)
+}
 
-	summedDeeps, _ := getSummedValues(deeps, 3)
+func getDepth(inputLines []string) (depths []int) {
+	for i := 0; i < len(inputLines); i++ {
+		depth, _ := strconv.Atoi(inputLines[i])
+		depths = append(depths, depth)
+	}
+	return
+}
 
-	answer2, _ := getNumberOfPositiveGradients(summedDeeps)
-	fmt.Println(fmt.Sprintf("Answer: [%d]", answer2))
+func getNumberOfPositiveGradients(depths []int, windowsSize int) (result int) {
+	result = 0
+	for i := 1; i < len(depths)-(windowsSize-1); i++ {
+		if sum(depths, i-1, windowsSize) < sum(depths, i, windowsSize) {
+			result++
+		}
+	}
+	return
+}
+
+func sum(input []int, offset int, windowsSize int) (sum int) {
+	for i := 0; i < windowsSize; i++ {
+		sum += input[offset+i]
+	}
+	return
 }
--- main.go
+++ main.go
@@ -2,16 +2,18 @@ package main
 
 import (
 	"fmt"
-	"nobody84/advent-of-code-2021/day7"
+	"nobody84/advent-of-code-2021/day1"
 )
 
 func main() {
 	fmt.Println("Advent of Code 2021")
-	// day1.Solve()
+	fmt.Printf("Day 1 - Part One: How many measurements are larger than the previous measurement? Answer: [%d]\n", day1.PartOne("day1/input.txt"))
+	fmt.Printf("Day 1 - Part Two: How many sums are larger than the previous sum? Answer: [%d]\n", day1.PartTwo("day1/input.txt"))
+
 	// day2.Solve()
 	// day3.Solve()
 	// day4.Solve()
 	// day5.Solve()
 	// day6.Solve()
-	day7.Solve()
+	// day7.Solve()
 }
