8638fd286c57d32063cb1265076efa518a7db7d2
Nobody <topmailcat@googlemail.com>
2021-12-08 21:53:34 +0100

    add test for day3

--- 2021/main.go
+++ 2021/main.go
@@ -10,12 +10,17 @@ func main() {
 	// fmt.Printf("Day 1 - Part One: How many measurements are larger than the previous measurement? Answer: [%d]\n", day1.PartOne("day1/input.txt"))
 	// fmt.Printf("Day 1 - Part Two: How many sums are larger than the previous sum? Answer: [%d]\n", day1.PartTwo("day1/input.txt"))
 
-	// day2.Solve()
-	// day3.Solve()
-	// day4.Solve()
-	// day5.Solve()
-	// day6.Solve()
-	// day7.Solve()
+	// fmt.Printf("Day 2 - Part One: What do you get if you multiply your final horizontal position by your final depth? Answer: [%d]\n", day2.PartOne("day2/input.txt"))
+	// fmt.Printf("Day 2 - Part Two: What do you get if you multiply your final horizontal position by your final depth? (With aim) Answer: [%d]\n", day2.PartTwo("day2/input.txt"))
+
+	// fmt.Printf("Day 3 - Part One: What is the power consumption of the submarine? Answer: [%d]\n", day3.PartOne("day3/input.txt"))
+	// fmt.Printf("Day 3 - Part One: What is the life support rating of the submarine? Answer: [%d]\n", day3.PartTwo("day3/input.txt"))
+
+	// fmt.Printf("Day 1 - Part One: How many measurements are larger than the previous measurement? Answer: [%d]\n", day1.PartOne("day1/input.txt"))
+	// fmt.Printf("Day 1 - Part Two: How many sums are larger than the previous sum? Answer: [%d]\n", day1.PartTwo("day1/input.txt"))
+
+	// fmt.Printf("Day 1 - Part One: How many measurements are larger than the previous measurement? Answer: [%d]\n", day1.PartOne("day1/input.txt"))
+	// fmt.Printf("Day 1 - Part Two: How many sums are larger than the previous sum? Answer: [%d]\n", day1.PartTwo("day1/input.txt"))
 
 	fmt.Printf("Day 8 - Part One: In the output values, how many times do digits 1, 4, 7, or 8 appear? Answer: [%d]\n", day8.PartOne("day8/input.txt"))
 	fmt.Printf("Day 8 - Part Two: What do you get if you add up all of the output values? Answer: [%d]\n", day8.PartTwo("day8/input.txt"))
