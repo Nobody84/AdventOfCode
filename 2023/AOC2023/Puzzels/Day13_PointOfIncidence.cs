9b3e6029ed34e8512d0e0edf31564dbef6fe4c9c
Nobody <topmailcat@googlemail.com>
2023-12-13 22:49:35 +0100

    no fix, just changes

--- 2023/AOC2023/Puzzels/Day13_PointOfIncidence.cs
+++ 2023/AOC2023/Puzzels/Day13_PointOfIncidence.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Runtime.InteropServices;
 using System.Security;
 using System.Text.RegularExpressions;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 public class Day13_PointOfIncidence
 {
@@ -39,6 +40,11 @@ public class Day13_PointOfIncidence
         blocks.Add(currentBlock);
         var mirrors = FindMirrors(blocks);
 
+        //foreach(var mirror in mirrors)
+        //{
+        //    PrintBlock(blocks[mirror.BlockId - 1], mirror);
+        //}
+
         return mirrors.Sum(m => m.Direction == Direction.Horizontal ? 100 * m.Position : m.Position);
     }
 
@@ -49,26 +55,32 @@ public class Day13_PointOfIncidence
         foreach (var rows in blocks)
         {
             blockNumber++;
-            var mirrorPosition = FindMirrorPosition(rows);
-            // If we found a vertical mirror, we don't need to check for a horizontal mirror
-            if (mirrorPosition != 0)
-            {
-                var verticalMirror = new Mirror(blockNumber, Direction.Vertical, mirrorPosition);
-                mirrors.Add(verticalMirror);
-                continue;
-            }
-
-            var columnNumbers = Enumerable.Range(0, rows[0].Length);
-            var columns = columnNumbers.Select((_, idx) => new string(rows.Select(row => row[idx]).ToArray())).ToList();
-            mirrorPosition = FindMirrorPosition(columns);
-            if (mirrorPosition != 0)
-            {
-                var horizontalMirror = new Mirror(blockNumber, Direction.Horizontal, mirrorPosition);
-                mirrors.Add(horizontalMirror);
-            }
+            mirrors.AddRange(FindMirrors(rows, blockNumber));
         }
 
         return mirrors;
+    }
+
+    private static IEnumerable<Mirror> FindMirrors(List<string> rows, int blockNumber)
+    {
+        var mirrorPosition = FindMirrorPosition(rows);
+        // If we found a vertical mirror, we don't need to check for a horizontal mirror
+        if (mirrorPosition != 0)
+        {
+            var verticalMirror = new Mirror(blockNumber, Direction.Vertical, mirrorPosition);
+            yield return verticalMirror;
+        }
+
+        var columnNumbers = Enumerable.Range(0, rows[0].Length);
+        var columns = columnNumbers.Select((_, idx) => new string(rows.Select(row => row[idx]).ToArray())).ToList();
+        mirrorPosition = FindMirrorPosition(columns);
+        if (mirrorPosition != 0)
+        {
+            var horizontalMirror = new Mirror(blockNumber, Direction.Horizontal, mirrorPosition);
+            yield return horizontalMirror;
+        }
+
+        yield return new Mirror(blockNumber, Direction.None, 0);
     }
 
     private static int FindMirrorPosition(List<string> lines)
@@ -131,8 +143,71 @@ public class Day13_PointOfIncidence
         Console.WriteLine();
     }
 
-    public ulong Part2()
+    public long Part2()
     {
-        return 0;
+        var lines = System.IO.File.ReadAllLines(@"Inputs\Day13.txt");
+
+        var blocks = new List<List<string>>();
+        var currentBlock = new List<string>();
+        foreach (var line in lines)
+        {
+            if (string.IsNullOrEmpty(line))
+            {
+                blocks.Add(currentBlock);
+                currentBlock = new List<string>();
+                continue;
+            }
+
+            currentBlock.Add(line);
+        }
+
+        blocks.Add(currentBlock);
+        var mirrors = new List<Mirror>();
+        var origMirrors = FindMirrors(blocks);
+
+        var blockNumber  = 0;
+        foreach (var block in blocks)
+        {
+            var blockMirrors = new List<Mirror>();
+            blockNumber++;
+            Console.WriteLine($"Block {blockNumber}");
+            var newBlock = block.Select(r => r.ToArray()).ToArray();
+            for (var y = 0; y < block.Count; y++)
+            {
+                var line = new List<char>();
+                for (var x = 0; x < block[0].Count(); x++)
+                {
+                    var original = newBlock[y][x];
+                    newBlock[y][x] = original == '.' ? 'X' : '+';
+                    PrintBlock(newBlock.Select(s => string.Join(null, s)).ToList(), new Mirror(0, Direction.None, 0));
+                    newBlock[y][x] = original == '.' ? '#' : '.';
+                    var newMirrors = FindMirrors(newBlock.Select(s => string.Join(null, s)).ToList(), blockNumber);
+
+                    foreach (var mirror in newMirrors)
+                    {
+                        if (mirror.Direction != Direction.None)
+                        {
+                            if (!origMirrors.Contains(mirror) && !blockMirrors.Contains(mirror))
+                            {
+                                blockMirrors.Add(mirror);
+                                newBlock[y][x] = original == '.' ? 'X' : '+';
+                                //PrintBlock(newBlock.Select(s => string.Join(null, s)).ToList(), mirror);
+                            }
+                        }
+                    }
+
+                    newBlock[y][x] = original;
+                }
+            }
+
+            mirrors.AddRange(blockMirrors);
+            if (blockMirrors.Count == 0)
+            { 
+                Console.WriteLine($"{blockNumber} No mirrors found");
+                PrintBlock(block, origMirrors.First());
+            }
+        }
+
+        return mirrors.Sum(m => m.Direction == Direction.Horizontal ? 100 * m.Position : m.Position);
     }
 }
