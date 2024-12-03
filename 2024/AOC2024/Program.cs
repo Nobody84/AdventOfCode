2930793105e9254e0e2c7e519f61c87814255009
Christian Drüke <drueke@tagitron.de>
2024-12-03 15:04:25 +0100

    2024 - Day1

---
+++ 2024/AOC2024/Program.cs
@@ -0,0 +1,7 @@
+﻿
+using AOC2024.Puzzels;
+
+var day1 = new Day1_HistorianHysteria();
+Console.WriteLine($"Day1 - Part1: {day1.Part1()}");
+Console.WriteLine($"Day1 - Part2: {day1.Part2()}");
+
---
+++ 2024/AOC2024/Puzzels/Day1_HistorianHysteria.cs
@@ -0,0 +1,36 @@
+﻿namespace AOC2024.Puzzels;
+
+public class Day1_HistorianHysteria
+{
+    public int Part1()
+    {
+        var lines = File.ReadLines("Inputs/Day1.txt");
+        var splits = lines.Select(l => l.Split("   ").Select(int.Parse));
+        var leftList = splits.Select(i => i.ElementAt(0)).Order().ToArray();
+        var rightList = splits.Select(i => i.ElementAt(1)).Order().ToArray();
+
+        var sum = 0;
+        for (var i = 0; i < leftList.Count(); i++)
+        {
+            sum += Math.Abs(leftList[i] - rightList[i]);
+        }
+
+        return sum;
+    }
+
+    public int Part2()
+    {
+        var lines = File.ReadLines("Inputs/Day1.txt");
+        var splits = lines.Select(l => l.Split("   ").Select(int.Parse));
+        var leftList = splits.Select(i => i.ElementAt(0));
+        var rightListGroups = splits.Select(i => i.ElementAt(1)).GroupBy(n => n).ToDictionary(g => g.Key, v => v.Count());
+
+        var sum = 0;
+        foreach (var number in leftList)
+        {
+            sum += number * (rightListGroups.ContainsKey(number) ? rightListGroups[number] : 0);
+        }
+
+        return sum;
+    }
+}
