f8dc9ea2b6fc47d1bab6e7336bad593d96c17e70
Christian Drüke <drueke@tagitron.de>
2024-12-04 12:28:23 +0100

    Day 3

--- 2024/AOC2024/Program.cs
+++ 2024/AOC2024/Program.cs
@@ -1,11 +1,15 @@
 ﻿
 using AOC2024.Puzzels;
 
-var day1 = new Day1_HistorianHysteria();
-Console.WriteLine($"Day1 - Part1: {day1.Part1()}");
-Console.WriteLine($"Day1 - Part2: {day1.Part2()}");
+//var day1 = new Day1_HistorianHysteria();
+//Console.WriteLine($"Day1 - Part1: {day1.Part1()}");
+//Console.WriteLine($"Day1 - Part2: {day1.Part2()}");
 
-var day2 = new Day2_RedNosedReports();
-Console.WriteLine($"Day2 - Part1: {day2.Part1()}");
-Console.WriteLine($"Day2 - Part2: {day2.Part2()}");
+//var day2 = new Day2_RedNosedReports();
+//Console.WriteLine($"Day2 - Part1: {day2.Part1()}");
+//Console.WriteLine($"Day2 - Part2: {day2.Part2()}");
+
+var day3 = new Day3_MullItOver();
+Console.WriteLine($"Day3 - Part1: {day3.Part1()}");
+Console.WriteLine($"Day3 - Part2: {day3.Part2()}");
 
---
+++ 2024/AOC2024/Puzzels/Day3_MullItOver.cs
@@ -0,0 +1,45 @@
+﻿namespace AOC2024.Puzzels;
+
+using System.Text.RegularExpressions;
+
+public class Day3_MullItOver
+{
+    private const string mullRegexPattern = @"mul\s*\(\s*(?<X>[\d]{1,3})\s*,(?<Y>[\d]{1,3})\s*\)";
+    private const string dontRegexPattern = @"don't\(\)(?:.|\n)*?do\(\)";
+    private const string lastDontRegexPattern = @"don't\(\)(?:.|\n)*";
+
+
+    public int Part1()
+    {
+        var count = 0;
+        var input = File.ReadAllText("Inputs/Day3.txt");
+        var matches = Regex.Matches(input, mullRegexPattern);
+
+        foreach(Match match in matches)
+        {
+            var x = int.Parse(match.Groups["X"].Value);
+            var y = int.Parse(match.Groups["Y"].Value);
+            count += x * y;
+        }
+
+        return count;
+    }
+
+    public int Part2()
+    {
+        var count = 0;
+        var input = File.ReadAllText("Inputs/Day3.txt");
+        var cleandInput = Regex.Replace(input, dontRegexPattern, string.Empty);
+        cleandInput = Regex.Replace(cleandInput, lastDontRegexPattern, string.Empty);
+        var matches = Regex.Matches(cleandInput, mullRegexPattern);
+
+        foreach (Match match in matches)
+        {
+            var x = int.Parse(match.Groups["X"].Value);
+            var y = int.Parse(match.Groups["Y"].Value);
+            count += x * y;
+        }
+
+        return count;
+    }
+}
