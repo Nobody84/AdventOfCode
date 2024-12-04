d946c4caf909617e50ad2cf4f088b8b7ce7c1066
Christian Drüke <drueke@tagitron.de>
2024-12-04 09:01:59 +0100

    Day 2 - Part 1

--- 2024/AOC2024/Program.cs
+++ 2024/AOC2024/Program.cs
@@ -5,3 +5,7 @@ var day1 = new Day1_HistorianHysteria();
 Console.WriteLine($"Day1 - Part1: {day1.Part1()}");
 Console.WriteLine($"Day1 - Part2: {day1.Part2()}");
 
+var day2 = new Day2_RedNosedReports();
+Console.WriteLine($"Day2 - Part1: {day2.Part1()}");
+Console.WriteLine($"Day2 - Part2: {day2.Part2()}");
+
---
+++ 2024/AOC2024/Puzzels/Day2_RedNosedReports.cs
@@ -0,0 +1,66 @@
+﻿namespace AOC2024.Puzzels;
+
+public class Day2_RedNosedReports
+{
+    public int Part1()
+    {
+        var lines = File.ReadLines("Inputs/Day2.txt");
+        var levelsList = lines.Select(l => l.Split(" ").Select(int.Parse).ToArray());
+
+        var count = 0;
+        foreach (var levels in levelsList)
+        {
+            var safe = true;
+            var previous = levels[0];
+            var increasing = levels[1] - levels[0] > 0;
+            var skipCount = 1;
+            for (var i = 1; i < levels.Length; i++)
+            {
+                var current = levels[i];
+                var difference = current - previous;
+                if (increasing)
+                {
+                    if (difference < 1 || difference > 3)
+                    {
+                        if (skipCount == 0)
+                        {
+                            skipCount++;
+                            continue;
+                        }
+
+                        safe = false;
+                        break;
+                    }
+                }
+                else
+                {
+                    if (difference > -1 || difference < -3)
+                    {
+                        if (skipCount == 0)
+                        {
+                            skipCount++;
+                            continue;
+                        }
+
+                        safe = false;
+                        break;
+                    }
+                }
+
+                previous = current;
+            }
+
+            if (safe)
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public int Part2()
+    {
+
+    }
+}
