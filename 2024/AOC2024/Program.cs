d9cc918b2bb185305758decb781d59abcff659c4
Christian Drüke <drueke@tagitron.de>
2024-12-05 11:42:48 +0100

    AOC 2024 Day5 Part 1

--- 2024/AOC2024/Program.cs
+++ 2024/AOC2024/Program.cs
@@ -5,9 +5,9 @@ using AOC2024.Puzzels;
 //Console.WriteLine($"Day1 - Part1: {day1.Part1()}");
 //Console.WriteLine($"Day1 - Part2: {day1.Part2()}");
 
-var day2 = new Day2_RedNosedReports();
-Console.WriteLine($"Day2 - Part1: {day2.Part1()}");
-Console.WriteLine($"Day2 - Part2: {day2.Part2()}");
+//var day2 = new Day2_RedNosedReports();
+//Console.WriteLine($"Day2 - Part1: {day2.Part1()}");
+//Console.WriteLine($"Day2 - Part2: {day2.Part2()}");
 
 //var day3 = new Day3_MullItOver();
 //Console.WriteLine($"Day3 - Part1: {day3.Part1()}");
@@ -16,4 +16,8 @@ Console.WriteLine($"Day2 - Part2: {day2.Part2()}");
 //var day4 = new Day4_CeresSearch();
 //Console.WriteLine($"Day4 - Part1: {day4.Part1()}");
 //Console.WriteLine($"Day4 - Part2: {day4.Part2()}");
+
+var day5 = new Day5_PrintQueue();
+Console.WriteLine($"Day5 - Part1: {day5.Part1()}");
+Console.WriteLine($"Day5 - Part2: {day5.Part2()}");
 
---
+++ 2024/AOC2024/Puzzels/Day5_PrintQueue.cs
@@ -0,0 +1,78 @@
+﻿namespace AOC2024.Puzzels;
+
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+public class Day5_PrintQueue
+{
+
+    record PageOrderRule(int Page1, int Page2);
+    record Update(HashSet<int> PageHashSet, List<int> Pages);
+
+    public int Part1()
+    {
+        var count = 0;
+        var inputLines = File.ReadLines("Inputs/Day5.txt");
+
+        var pageOrderRules = new List<PageOrderRule>();
+        var updates = new List<Update>();
+
+        var pageOrderRulesProcessed = false;
+        foreach (var line in inputLines)
+        {
+            if (string.IsNullOrEmpty(line))
+            {
+                pageOrderRulesProcessed = true;
+                continue;
+            }
+
+            if (!pageOrderRulesProcessed)
+            {
+                var pages = line.Split("|").Select(int.Parse).ToArray();
+                pageOrderRules.Add(new PageOrderRule(pages[0], pages[1]));
+            }
+            else
+            {
+                var pages = line.Split(",").Select(int.Parse).ToList();
+                updates.Add(new Update(pages.ToHashSet(), pages));
+            }
+        }
+
+        var validUpdates = new List<Update>();
+        foreach (var update in updates)
+        {
+            var updateOrderOk = true;
+            var relevantOrderRules = pageOrderRules.Where(r => update.PageHashSet.Contains(r.Page1) && update.PageHashSet.Contains(r.Page2)).ToList();
+            foreach (var orderRule in relevantOrderRules)
+            {
+                var posPage1 = update.Pages.IndexOf(orderRule.Page1);
+                var posPage2 = update.Pages.IndexOf(orderRule.Page2);
+                if (posPage1 > posPage2)
+                {
+                    updateOrderOk = false;
+                    break;
+                }
+            }
+
+            if (updateOrderOk)
+            {
+                validUpdates.Add(update);
+            }
+        }
+
+        foreach (var validUpdate in validUpdates)
+        {
+            count += validUpdate.Pages[validUpdate.Pages.Count / 2];
+        }
+
+        return count;
+    }
+
+    public int Part2()
+    {
+        var count = 0;
+        var inputLines = File.ReadLines("Inputs/Day5.txt").ToList();
+
+        return count;
+    }
+}
