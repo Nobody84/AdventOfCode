9f1ad22e640fc18a6bebd83f61b80cc9b8745234
Nobody <topmailcat@googlemail.com>
2024-12-10 08:14:31 +0100

    AOC 2024 Day9 Part 2 (WIP)

--- 2024/AOC2024/Puzzels/Day9_DiskFragmenter.cs
+++ 2024/AOC2024/Puzzels/Day9_DiskFragmenter.cs
@@ -2,17 +2,27 @@
 
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Runtime.ExceptionServices;
 using System.Text;
-using System.Text.RegularExpressions;
 
 public class Day9_DiskFragmenter : PuzzelBase
 {
     private int[] input = [];
 
+    private record File
+    {
+        public int StartIdx { get; init; }
+        public int Id { get; init; }
+        public int Length { get; set; }
+    };
+
+    private record FreeSpace
+    {
+        public int StartIdx { get; init; }
+        public int Length { get; init; }
+    };
+
     public Day9_DiskFragmenter()
-        : base(9, "Disk Fragmenter")
+    : base(9, "Disk Fragmenter")
     {
     }
 
@@ -71,6 +81,36 @@ public class Day9_DiskFragmenter : PuzzelBase
 
     protected override object Part2()
     {
+        var uncompressedFile = new List<int>();
+
+        var files = new List<File>();
+        var freeSpaces = new List<FreeSpace>();
+        var fileId = 0;
+        var uncompressdIdx = 0;
+        for (int i = 0; i < input.Length; i++)
+        {
+            var value = input[i];
+            if (i % 2 == 0)
+            {
+                // value is file length
+                files.Add(new File { StartIdx = uncompressdIdx, Id = fileId, Length = value });
+                fileId++;
+            }
+            else
+            {
+                // value is freespace length, freespace is represented by -1
+                freeSpaces.Add(new FreeSpace { StartIdx = uncompressdIdx, Length = value });
+            }
+
+            uncompressdIdx += value;
+        }
+
+        files.Reverse();
+        foreach (var file in files)
+        {
+            file.Length = 23;
+        }
+
         return 0;
     }
 
