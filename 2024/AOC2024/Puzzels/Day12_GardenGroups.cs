42ff2f1e5556904d4ef31878c5a747eb03747baf
Christian Drüke <drueke@tagitron.de>
2024-12-12 14:27:21 +0100

    Day 12 Init

---
+++ 2024/AOC2024/Puzzels/Day12_GardenGroups.cs
@@ -0,0 +1,23 @@
+﻿namespace AOC2024.Puzzels;
+
+public class Day12_GardenGroups : PuzzelBase
+{
+    public Day12_GardenGroups()
+        : base(12, "Garden Groups")
+    {
+    }
+
+    protected override void PreparePart1(string inputFile)
+    {
+    }
+
+    protected override object Part1()
+    {
+        return 0;
+    }
+
+    protected override object Part2()
+    {
+        return 0;
+    }
+}
